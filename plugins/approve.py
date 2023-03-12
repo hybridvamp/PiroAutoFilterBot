23e98b3459dd8710d082bf98820ef95504626216
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-03-12 20:58:34 +0400

    user_id=user.id

--- plugins/approve.py
+++ plugins/approve.py
@@ -12,4 +12,4 @@ async def autoapprove(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=user, text=TEXT.format(mention=user.mention, title=chat.title))
+        await client.send_message(user_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title))
