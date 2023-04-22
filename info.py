1d236f692a9c3aa7d3d1b254e2580c0a0c3e1b78
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-04-22 18:24:02 +0400

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,9 @@
 import re
 from os import environ
-from Script import script 
+from Script import script
+from dotenv import load_dotenv
+
+load_dotenv()
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
