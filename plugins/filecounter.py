0d18054e47b9f8391a8ab474519b57979710bca4
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-04-26 20:43:30 +0400

    filecounter.py

---
+++ plugins/filecounter.py
@@ -0,0 +1,27 @@
+import pymongo
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from info import DATABASE_URI, DATABASE_NAME, CHAT_ID, POST_ID
+
+myclient = pymongo.MongoClient(DATABASE_URI)
+mydb = myclient[DATABASE_NAME]
+
+@client.on_message(filters.chat(CHAT_ID) & (filters.document | filters.video | filters.audio | filters.photo | filters.animation))
+async def handle_new_files(_, message: Message):
+    # Increment the total files count in the database
+        mycol = mydb["file_counts"]
+        myquery = {"_id": "total_files_sent"}
+        newvalues = {"$inc": {"count": 1}}
+        mycol.update_one(myquery, newvalues, upsert=True)
+                        
+        # Get the current total files count from the database
+        count = mycol.find_one(myquery)["count"]
+                                    
+        # Edit the post in the chat with the new total files count
+        await client.edit_message_text(
+            chat_id=CHAT_ID,
+            message_id=POST_ID,
+            text=f"Total files sent: {count}"
+        )
+
