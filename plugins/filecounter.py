f4a94c23e609ed860b20e2562a2afa05ca750857
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-04-27 19:05:21 +0400

    new bot for edit

--- plugins/filecounter.py
+++ plugins/filecounter.py
@@ -4,12 +4,23 @@ from pyrogram.types import Message
 
 from info import DATABASE_URI, DATABASE_NAME, CHAT_ID
 
+API_ID = 3881325
+API_HASH = "1ebf12e67222683abe1c01f0209b79df"
+BOT_TOKEN = "5725763519:AAErtokI_ODgqDA6QQLpCxZJBUoj1NvmgI8"
+
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 POST_ID = 5
 
-@Client.on_message(filters.chat(CHAT_ID))
-async def handle_new_files(client, message: Message):
+@app.on_message(filters.chat(CHAT_ID))
+async def handle_new_files(_, message: Message):
     # Increment the total files count in the database
         mycol = mydb["file_counts"]
         myquery = {"_id": "total_files_sent"}
@@ -20,9 +31,10 @@ async def handle_new_files(client, message: Message):
         count = mycol.find_one(myquery)["count"]
                                     
         # Edit the post in the chat with the new total files count
-        await client.edit_message_text(
+        await app.edit_message_text(
             chat_id=CHAT_ID,
             message_id=POST_ID,
             text=f"Total files sent: {count}"
         )
 
+app.run()
